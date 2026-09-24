eedc59cd1ef62246c73834e646d94462f3ea4b7f
yoyo021608 <3118555011@qq.com>
2026-09-24 19:46:32 +0800

    ci: configure black for Python 3.12

--- server/config.py
+++ server/config.py
@@ -29,11 +29,7 @@ class Settings(BaseSettings):
 
     @property
     def cors_origin_list(self) -> list[str]:
-        return [
-            origin.strip()
-            for origin in self.cors_origins.split(",")
-            if origin.strip()
-        ]
+        return [origin.strip() for origin in self.cors_origins.split(",") if origin.strip()]
 
 
 @lru_cache
